1dd6f2aadce0406257c047ae065778a340a2fd89
Wee <fi3ework@gmail.com>
2019-03-15 15:34:37 +0800

    test: add base passed tests

--- __test__/LiveRoute.test.tsx
+++ __test__/LiveRoute.test.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import * as PropTypes from 'prop-types'
 import { MemoryRouter, Route, Router } from 'react-router'
 import { createMemoryHistory } from 'history'
 
